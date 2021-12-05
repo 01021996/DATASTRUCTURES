a6719b7ea13cd8b57e84b895e1ea8172e9f0c0ea
Saksham Jain <jainsaksham0102@gmail.com>
2021-12-05 19:01:46 +0530

    added digitFrequency.java

---
+++ 2DArrays/digitFrequency.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class digitFrequency
+{
+    public static void main(String[] args)
+    {
+        System.out.println("enter ");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int d = scn.nextInt();
+        int f = getDigitFrequency(n, d);
+        System.out.println(f);
+    }
+
+    public static int getDigitFrequency(int n, int d)
+	{
+		int count=0;
+		while(n!=0)
+		{
+			int rem=n%10;
+			if(rem==d) count++;
+			n=n/10;
+		}
+        
+		return count;
+    }
+}
