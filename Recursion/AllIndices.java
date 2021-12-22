7a6f3d84fd8f2d7da851cd745a4c8debaa4aa559
Saksham Jain <jainsaksham0102@gmail.com>
2021-12-22 13:28:09 +0530

    allindices.java

---
+++ Recursion/AllIndices.java
@@ -0,0 +1,46 @@
+public class AllIndices 
+{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int x = Integer.parseInt(br.readLine());
+        int[] iarr = allIndices(arr, x, 0, 0);
+
+        if(iarr.length == 0){
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i < iarr.length; i++){
+            System.out.println(iarr[i]);
+        }
+    }
+
+    public static int[] allIndices(int[] arr, int x, int idx, int count) 
+    {
+        if(idx==arr.length)
+        {
+            int [] base=new int[count];
+            return base;
+        }
+         //counting the total no of x elements
+        if(arr[idx]==x)
+            count++;
+
+        int [] recAns=allIndices(arr,x,idx+1,count);
+
+        //while returning from the base case
+        if(arr[idx]==x)
+        {
+            recAns[count-1]=idx;
+        }
+
+        return recAns;
+
+    }
+}
